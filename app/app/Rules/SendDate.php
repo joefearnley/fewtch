463502d3530cf99d9bc0f54a7dec8266e0fc2d8a
Joe Fearnley <joe.fearnley@gmail.com>
2025-10-13 22:52:26 -0400

    Updating send date validation rule

--- app/app/Rules/SendDate.php
+++ app/app/Rules/SendDate.php
@@ -4,9 +4,22 @@ namespace App\Rules;
 
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Validation\Rules\Date;
 
 class SendDate implements ValidationRule
 {
+    protected $sendIn;
+    protected $sendDate;
+
+    /**
+     * Create a new rule instance.
+     */
+    public function __construct($sendIn, $sendDate)
+    {
+        $this->sendIn = $sendIn;
+        $this->sendDate = $sendDate;
+    }
+
     /**
      * Run the validation rule.
      *
@@ -14,6 +27,19 @@ class SendDate implements ValidationRule
      */
     public function validate(string $attribute, mixed $value, Closure $fail): void
     {
-        //
+        if (empty($this->sendIn) && empty($this->sendDate)) {
+            $fail('Please select a send date or interval.');
+        }
+
+        if (!empty($this->sendDate)) {
+
+            $dateRule = (new Date)->afterOrEqual('today');
+            $result = $dateRule->passes($attribute, $this->sendDate);
+
+
+            $fail('Please select either a send date or an interval, not both.');
+
+
+        }
     }
 }
