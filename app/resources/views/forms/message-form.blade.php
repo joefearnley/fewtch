84cebeba192674e67551356702af49466e8035e6
Joe Fearnley <joe.fearnley@gmail.com>
2025-10-29 19:24:52 -0400

    Adding correct validation to main message form

--- app/resources/views/forms/message-form.blade.php
+++ app/resources/views/forms/message-form.blade.php
@@ -1,14 +1,3 @@
-
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
 <div class="bg-white dark:bg-gray-800 rounded-lg shadow-sm p-6 border border-gray-200 dark:border-gray-700">
     <form method="POST" action="{{ route('message.store') }}" class="space-y-3">
         @csrf
@@ -18,9 +7,9 @@
             id="message-body"
             rows="10"
             placeholder="Compose your message here..."></textarea>
-            @error('content')
-                <div class="text-red-700 text-sm mb-4">{{ $message }}</div>
-            @enderror
+        @error('content')
+            <div class="text-red-700 dark:text-red-400 text-sm mb-4">{{ $message }}</div>
+        @enderror
         <div class="flex flex-col md:flex-row md:items-center md:justify-between mt-4">
             <div>
                 <div class="flex flex-col md:flex-row md:items-start">
@@ -42,14 +31,9 @@
                         <input type="date" id="send_date" placeholder="" name="send_date" class="w-full px-4 py-1.5 rounded-lg text-gray-700 dark:text-gray-300 bg-gray-50 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-sky-500 focus:border-transparent" value="{{ old('send_date') }}">
                     </div>
                 </div>
-                @if ($errors->has('send_in') || $errors->has('send_date'))
-                    @foreach ($errors->get('send_date') as $message)
-                        <div class="mt-2 text-red-700 dark:text-red-400 text-sm pb-5">{{ $message }}</div>
-                    @endforeach
-                    @foreach ($errors->get('send_in') as $message)
-                        <div class="mt-2 text-red-700 dark:text-red-400 text-sm pb-5">{{ $message }}</div>
-                    @endforeach
-                @endif
+                @error('send_date')
+                    <div class="mt-2 text-red-700 dark:text-red-400 text-sm pb-5">{{ $message }}</div>
+                @enderror
             </div>
             <div class="flex justify-end mt-4 md:mt-0">
                 <x-button>{{ __('Fewtch It!') }}</x-button>
