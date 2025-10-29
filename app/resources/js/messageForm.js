ce336f6faf1072c218b41d55df37265e6f24737d
Joe Fearnley <joe.fearnley@gmail.com>
2025-10-29 19:21:04 -0400

    Adding funtionality to select time in future and update date

--- app/resources/js/messageForm.js
+++ app/resources/js/messageForm.js
@@ -1,36 +1,52 @@
 
-const sendInOptions = document.getElementById('send-in-options');
-const buttons = document.querySelectorAll('#send-in-options button');
-const sendDateInput = document.getElementById('send-date');
+const sendInButtons = document.querySelectorAll('#send-in-options button');
+const sendDateInput = document.querySelector('#send_date');
 
-sendInOptions.addEventListener('click', (event) => {
-    event.preventDefault();
+sendInButtons.forEach(sendInButton => {
+    sendInButton.addEventListener('click', (event) => {
+        event.preventDefault();
 
-    // Remove active class from all buttons
-    const buttons = document.querySelectorAll('#send-in-options button');
-    buttons.forEach(button => {
-        button.classList.remove('bg-jule-500', 'text-white');
-        button.classList.add('bg-zinc-100', 'hover:bg-zinc-200', 'dark:bg-zinc-700', 'dark:hover:bg-zinc-600', 'text-foreground');
+        // remove active class/state from all buttons
+        sendInButtons.forEach(button => {
+            button.classList.remove('bg-jule-500', 'text-white');
+            button.classList.add('bg-zinc-100', 'hover:bg-zinc-200', 'dark:bg-zinc-700', 'dark:hover:bg-zinc-600', 'text-foreground');
+        });
+
+        // Add active class to the clicked button
+        sendInButton.classList.add('bg-jule-500', 'text-white');
+        sendInButton.classList.remove('bg-zinc-100', 'hover:bg-zinc-200', 'dark:bg-zinc-700', 'dark:hover:bg-zinc-600', 'text-foreground');
+
+        // set the date value based on what button is selected
+        const sendInValue = sendInButton.textContent.trim();
+        const daysInFuture = getDaysFromInput(sendInValue);
+
+        if (daysInFuture) {
+            const currentDate = new Date();
+            currentDate.setDate(currentDate.getDate() + daysInFuture);
+            const formattedDate = currentDate.toISOString().split('T')[0];
+
+            sendDateInput.value = formattedDate;
+        }
     });
-
-    // Add active class to the clicked button
-    event.target.classList.add('bg-jule-500', 'text-white');
-    event.target.classList.remove('bg-zinc-100', 'hover:bg-zinc-200', 'dark:bg-zinc-700', 'dark:hover:bg-zinc-600', 'text-foreground');
-
-    // Update date field
-    console.log(event.target.textContent);
 });
 
 sendDateInput.addEventListener('change', (event) => {
-    console.log('date changed');
-
     // Clear selection of send-in buttons
-    const buttons = document.querySelectorAll('#send-in-options button');
-    buttons.forEach(button => {
+    sendInButtons.forEach(button => {
         button.classList.remove('bg-jule-500', 'text-white');
         button.classList.add('bg-zinc-100', 'hover:bg-zinc-200', 'dark:bg-zinc-700', 'dark:hover:bg-zinc-600', 'text-foreground');
     });
-
-    // Clear hidden input value
-    document.getElementById('send-in').value = '';
 });
+
+const getDaysFromInput = (sendInInput) => {
+    const mapping = {
+        '3 Months': 90,
+        '6 Months': 180,
+        '1 Year': 365,
+        '3 Years': 1095,
+        '5 Years': 1825,
+        '10 Years': 3650
+    };
+
+    return mapping[sendInInput] || null;
+};
