03e71a29f92217c6f593e74116b59b2d9a43553d
Joe Fearnley <joe.fearnley@gmail.com>
2026-01-15 21:41:29 -0500

    Adding more tests

---
+++ app/tests/Feature/Message/MessageCancelRequeueTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\User;
+use App\Models\Message;
+
+test('authenticated user can requeue their message', function () {
+    $user = User::factory()->create();
+
+    $message = Message::factory()->create([
+        'user_id' => $user->id,
+        'subject' => 'To be deleted',
+        'content' => 'Delete me',
+        'send_date' => now()->addDays(3)->toDateString(),
+    ]);
+
+    $this->actingAs($user)
+        ->delete(route('messages.destroy', $message))
+        ->assertRedirect(route('dashboard'));
+
+    $this->assertDatabaseMissing('messages', [
+        'id' => $message->id,
+    ]);
+});
+
+test('authenticated user cannot delete another user\'s message', function () {
+    $user = User::factory()->create();
+    $otherUser = User::factory()->create();
+
+    $message = Message::factory()->create([
+        'user_id' => $otherUser->id,
+        'subject' => 'To be deleted',
+        'content' => 'Delete me',
+        'send_date' => now()->addDays(3)->toDateString(),
+    ]);
+
+    $this->actingAs($user)
+        ->delete(route('messages.destroy', $message))
+        ->assertStatus(403);
+});
