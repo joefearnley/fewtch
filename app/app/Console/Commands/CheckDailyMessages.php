a485e773dbd05eb4d7b04ddd3a22153a1911b94e
Joe Fearnley <joe.fearnley@gmail.com>
2026-01-23 22:06:23 -0500

    getting there

--- app/app/Console/Commands/CheckDailyMessages.php
+++ app/app/Console/Commands/CheckDailyMessages.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Message;
 
 class CheckDailyMessages extends Command
 {
@@ -25,6 +26,14 @@ class CheckDailyMessages extends Command
      */
     public function handle()
     {
-        //
+        // Get today's date
+        $today = now()->toDateString();
+
+        // get message that the send_at date is today
+        $messages = Message::whereDate('send_at', $today)->get();
+
+        foreach ($messages as $message) {
+            $this->info("Message ID {$message->id} is scheduled to be sent today.");
+        }
     }
 }
