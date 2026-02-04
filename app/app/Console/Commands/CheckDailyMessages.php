21939f723c5e7de6617e62446546b7700bd9e101
Joe Fearnley <joe.fearnley@gmail.com>
2026-02-03 23:11:25 -0500

    roughing in email template

--- app/app/Console/Commands/CheckDailyMessages.php
+++ app/app/Console/Commands/CheckDailyMessages.php
@@ -36,8 +36,6 @@ class CheckDailyMessages extends Command
             $this->info("Message ID {$message->id} is scheduled to be sent today.");
             $this->info("Sending Message ID {$message->id}....");
 
-            // create 'email from email template and send it
-            // For demonstration, we'll just print a message
 
         }
     }
