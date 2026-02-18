551580d37a9bbf60148adc89e74ed591bb80a699
Joe Fearnley <joe.fearnley@gmail.com>
2026-02-18 17:19:05 -0500

    Schedule SendMessages to run daily

--- app/routes/console.php
+++ app/routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Console\Commands\SendMessages;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+// Schedule the SendMessages command to run daily
+Schedule::call(SendMessages::class)
+    ->purpose('Check for and sendmessages')
+    ->daily();
